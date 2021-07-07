74798ee7588c4402a03233cfa57fd9644f173273
bbung95 <tjd123123@naver.com>
2021-07-07 13:44:32 +0900

    Create

---
+++ MoopiProject/src/main/java/com/moopi/mvc/MoopiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.moopi.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoopiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoopiProjectApplication.class, args);
+	}
+
+}
---
+++ MoopiProject/src/test/java/com/moopi/mvc/MoopiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moopi.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoopiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
