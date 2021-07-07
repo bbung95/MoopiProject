19d3342338234e53d48d8acd06b4429d4df07149
ouneno <ouneno@naver.com>
2021-07-08 01:44:29 +0900

    youla : 2021-07-08 01:44
    UserService, UserServiceImpl, UserDao 일부작성완료

--- MoopiProject/src/main/java/com/moopi/mvc/service/user/UserService.java
+++ MoopiProject/src/main/java/com/moopi/mvc/service/user/UserService.java
@@ -1,5 +1,25 @@
 package com.moopi.mvc.service.user;
 
+import java.util.Map;
+
+import com.moopi.mvc.common.Search;
+import com.moopi.mvc.service.domain.User;
+
 public interface UserService {
+	
+	// 회원 가입
+	public void addUser(User user) throws Exception;
+	
+	// 회원 조회
+	public User getUser(String userId) throws Exception;
+	
+	// 회원 리스트
+	public Map<String, Object> getUserList(Search search) throws Exception;
+	
+	// 회원 수정
+	public void updateUser(User user) throws Exception;
+	
+	// 회원 ID 중복 확인
+	// public boolean checkDuplication(String userId) throws Exception;
 
 }
