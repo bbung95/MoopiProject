ccf6c0b72ad34ff921706a618a69e9d24c94c9c5
bit <bit@DESKTOP-INJFT3B>
2021-07-08 20:35:26 +0900

    2021-07-08 20:33 박율아
    @addUser 작성완료, getUser 작성예정
    UserMapper.xml
    
    @Mapper 형식에 맞춰서 하단 수정보완 및 삭제
    UserServiceImpl.java / UserMapper.java
    userDao.java / UserDaoImpl.java

---
+++ MoopiProject/src/main/java/com/moopi/mvc/service/user/UserMapper.java
@@ -0,0 +1,18 @@
+//package com.moopi.mvc.service.user;
+//
+//import org.apache.ibatis.annotations.Mapper;
+//import org.apache.ibatis.annotations.Param;
+//
+//import com.moopi.mvc.service.domain.User;
+//
+//@Mapper
+//public interface UserMapper {
+//	
+//	User addUser(User user);
+//	
+//	User getUser(@Param("userId") String userId);
+//	
+//	// User getUserList
+//	
+//	User updateUser(User user);
+//}
