efa8d8dec12804e86da978005ff540de498ed304
krKanki <tkdgus403@naver.com>
2021-07-08 20:45:44 +0900

    test

---
+++ MoopiProject/src/main/java/com/moopi/mvc/service/moim/MoimMapper.java
@@ -0,0 +1,23 @@
+package com.moopi.mvc.service.moim;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.moopi.mvc.common.Search;
+import com.moopi.mvc.service.domain.Moim;
+
+@Mapper
+public interface MoimMapper {
+
+	//public void addMoim(Moim moim) throws Exception;
+	
+	//public List<Moim> getMoimList(Search search) throws Exception;
+	
+	public Moim getMoim(@Param("mmNo") int mmNo);
+	
+	//public void updateMoim(Moim moim) throws Exception;
+	
+	//public int getTotalCount(Search search) throws Exception;
+}
