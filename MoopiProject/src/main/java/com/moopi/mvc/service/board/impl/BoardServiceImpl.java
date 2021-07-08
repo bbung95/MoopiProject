f31294d6abd5a65fe9afb70b4e98a96c1e78b206
bit <bit@DESKTOP-MNEP9O9>
2021-07-08 14:44:28 +0900

    ksh

--- MoopiProject/src/main/java/com/moopi/mvc/service/board/impl/BoardServiceImpl.java
+++ MoopiProject/src/main/java/com/moopi/mvc/service/board/impl/BoardServiceImpl.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.moopi.mvc.service.board.impl;
 
 import java.util.HashMap;
@@ -66,69 +65,3 @@ public class BoardServiceImpl implements BoardService {
 	}
 
 }
-=======
-//package com.moopi.mvc.service.board.impl;
-//
-//import java.util.HashMap;
-//import java.util.List;
-//import java.util.Map;
-//
-//import com.moopi.mvc.common.Search;
-//import com.moopi.mvc.service.board.BoardDao;
-//import com.moopi.mvc.service.board.BoardService;
-//import com.moopi.mvc.service.domain.Board;
-//
-//
-//
-//@Service("boardServiceImpl")
-//public class BoardServiceImpl implements BoardService {
-//
-//	@Autowired
-//	@Qualifier("boardDaoImpl")
-//	private BoardDao boardDao;
-//	
-//	public void setBoardDao(BoardDao boardDao) {
-//		this.boardDao = boardDao;
-//	}
-//	
-//	public BoardServiceImpl(){
-//		System.out.println(this.getClass());
-//	}
-//	
-//	
-//	@Override
-//	public void addBoard(Board board) throws Exception {
-//		// TODO Auto-generated method stub
-//		boardDao.insertBoard(board);
-//
-//	}
-//
-//	@Override
-//	public Board getBoard(int boardNo) throws Exception {
-//		// TODO Auto-generated method stub
-//		return boardDao.findBoard(boardNo);
-//	}
-//
-//	@Override
-//	public Map<String, Object> getBoardList(Search search) throws Exception {
-//		
-//		List<Board> list= boardDao.getBoardList(search);
-//		int totalCount =  boardDao.getTotalCount(search); 
-//		
-//		Map<String, Object> map = new HashMap<String, Object>();
-//		System.out.println(search+" serviceimpl");
-//		map.put("list", list);
-//		map.put("totalCount", totalCount);
-//		
-//		return map;
-//	}
-//
-//	@Override
-//	public void updateBoard(Board board) throws Exception {
-//		
-//		boardDao.updateBoard(board);
-//
-//	}
-//
-//}
->>>>>>> refs/remotes/origin/master
