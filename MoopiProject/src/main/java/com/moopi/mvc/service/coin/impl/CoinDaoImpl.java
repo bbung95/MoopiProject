174de618b33a445d3f7fbf59e3ff8691a01f038b
guddn <guddn@LAPTOP-65FJECOJ>
2021-07-08 12:21:35 +0900

    J :: flash,payment,coin,letter의 domain,Dao,service,DaoImpl, ServiceImpl 작성완료

--- MoopiProject/src/main/java/com/moopi/mvc/service/coin/impl/CoinDaoImpl.java
+++ MoopiProject/src/main/java/com/moopi/mvc/service/coin/impl/CoinDaoImpl.java
@@ -1,18 +1,19 @@
-package com.moopi.mvc.service.payment.impl;
+package com.moopi.mvc.service.coin.impl;
 
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
+import org.eclipse.jdt.internal.compiler.ast.ThisReference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.moopi.mvc.common.Search;
-import com.moopi.mvc.service.domain.Payment;
-import com.moopi.mvc.service.payment.PaymentDao;
+import com.moopi.mvc.service.coin.CoinDao;
+import com.moopi.mvc.service.domain.Coin;
 
-@Service("paymentDaoImpl")
-public class PaymentDaoService implements PaymentDao{
+@Service("coinDaoImpl")
+public class CoinDaoImpl implements CoinDao{
 
 	@Autowired
 	@Qualifier("sqlSessionTemplate")
@@ -23,30 +24,30 @@ public class PaymentDaoService implements PaymentDao{
 	}
 	
 	//Constructor
-	public PaymentDaoService() {
+	public CoinDaoImpl() {
 		System.out.println(this.getClass());
 	}
 
 	@Override
-	public void insertPayment(Payment payment) throws Exception {
+	public void insertCoin(Coin coin) throws Exception {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public List<Payment> getPaymentList(Search search) throws Exception {
+	public List<Coin> getCoinList(Search search) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Payment findPayment(int paymentNo) throws Exception {
+	public Coin findCoin(int coinNo) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void updatePayment(Payment payment) throws Exception {
+	public void updateCoin(Coin coin) throws Exception {
 		// TODO Auto-generated method stub
 		
 	}
