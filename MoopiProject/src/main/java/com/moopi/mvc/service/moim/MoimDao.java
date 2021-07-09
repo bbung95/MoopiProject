a693ecacb8a44e3552e953e9e2823172b82313b0
treefrog18 <65808975+treefrog18@users.noreply.github.com>
2021-07-09 20:52:47 +0900

    CYU:addMoim Test 완료

--- MoopiProject/src/main/java/com/moopi/mvc/service/moim/MoimDao.java
+++ MoopiProject/src/main/java/com/moopi/mvc/service/moim/MoimDao.java
@@ -10,13 +10,13 @@ import com.moopi.mvc.service.domain.Moim;
 @Mapper
 public interface MoimDao {
 
-	//public void addMoim(Moim moim) throws Exception;
+	public void addMoim(Moim moim) throws Exception;
 	
 	//public List<Moim> getMoimList(Search search) throws Exception;
 	
 	public Moim getMoim(@Param("mmNo") int mmNo);
 	
-	//public void updateMoim(Moim moim) throws Exception;
+	public void updateMoim(Moim moim) throws Exception;
 	
 	//public int getTotalCount(Search search) throws Exception;
 }
