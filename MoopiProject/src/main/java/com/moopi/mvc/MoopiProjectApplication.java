0f32536dbb0c9b5282d9998d957cb8e8f59496d0
bbung95 <tjd123123@naver.com>
2021-07-09 17:11:35 +0900

    2021-07-09 bbung
    commonApplicationTest select/insert

--- MoopiProject/src/main/java/com/moopi/mvc/MoopiProjectApplication.java
+++ MoopiProject/src/main/java/com/moopi/mvc/MoopiProjectApplication.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class MoopiProjectApplication {
 
 	public static void main(String[] args) {
