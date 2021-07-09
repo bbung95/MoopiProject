29cdc8fcc6782a4ad3e5abbd84995103bb04389d
treefrog18 <65808975+treefrog18@users.noreply.github.com>
2021-07-09 19:48:18 +0900

    CYU:이것으로 개인모듈만 작성하고, 타인이 작성하고있을것만같은 부분은 건드리지 맙시다.

--- MoopiProject/src/main/java/com/moopi/mvc/web/user/UserController.java
+++ MoopiProject/src/main/java/com/moopi/mvc/web/user/UserController.java
@@ -1,78 +1,3 @@
-package com.moopi.mvc.web.user;
-
-import java.util.List;
-
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.moopi.mvc.service.domain.User;
-
-//@Controller
-//@RequestMapping("/user/*")
-public class UserController {
-
-	//@Autowired
-	//@Qualifier("userServiceImpl")
-	//private UserServiceImpl userService;
-
-	public UserController() {
-		System.out.println(this.getClass());
-	}
-
-	// @Value("#{commonProperties['pageUnit']}")
-	// int pageUnit;
-	// @Value("#{commonProperties['pageSize']}")
-	// int pageSize;
-
-	// 로그인
-	@RequestMapping(value = "login", method = RequestMethod.POST)
-	public String login(@ModelAttribute("user") User user, HttpSession session) throws Exception {
-		// 2021-07-08 12:37 : VO쪽 getter, setter Annotaion지정했으나, 끌고오지 못함 알아봐야함
-		User dbUser = userService.getUser(user.getUserId());
-		return "redirect:/index";
-	}
-
-	// 로그아웃
-	@RequestMapping(value = "logout", method = RequestMethod.GET)
-	public String logout(HttpSession session) throws Exception {
-		session.invalidate();
-
-		return "redirect:/main.jsp";
-	}
-
-	// 회원가입
-	@RequestMapping(value = "addUser", method = RequestMethod.POST)
-	public String addUser() throws Exception {
-
-		// userService.addUser(user);
-
-		return "redirect:/user/addUserInfo.jsp";
-	}
-
-	// 회원가입 이후 추가 정보입력
-	@RequestMapping(value = "addUserInfo", method = RequestMethod.POST)
-	public String addUserInfo() throws Exception {
-		return "redirect:/main.jsp";
-	}
-
-//	public void addUser(User user) throws Exception;
-//	
-//	public User getUser(String userId) throws Exception;
-//	
-//	public List<User> getUserList(Search search) throws Exception;
-//	
-//	public void updateUser(User user) throws Exception;
-//	
-//	public int getTotalCount(Search search) throws Exception;
-
-}
-
 //package com.moopi.mvc.web.user;
 //
 //import java.util.List;
@@ -87,15 +12,14 @@ public class UserController {
 //import org.springframework.web.bind.annotation.RequestMethod;
 //
 //import com.moopi.mvc.service.domain.User;
-//import com.moopi.mvc.service.user.UserService;
 //
-//@Controller
-//@RequestMapping("/user/*")
+////@Controller
+////@RequestMapping("/user/*")
 //public class UserController {
 //
-//	@Autowired
-//	@Qualifier("userServiceImpl")
-//	private UserService userService;
+//	//@Autowired
+//	//@Qualifier("userServiceImpl")
+//	//private UserServiceImpl userService;
 //
 //	public UserController() {
 //		System.out.println(this.getClass());
@@ -148,4 +72,80 @@ public class UserController {
 ////	public int getTotalCount(Search search) throws Exception;
 //
 //}
-
+//
+////package com.moopi.mvc.web.user;
+////
+////import java.util.List;
+////
+////import javax.servlet.http.HttpSession;
+////
+////import org.springframework.beans.factory.annotation.Autowired;
+////import org.springframework.beans.factory.annotation.Qualifier;
+////import org.springframework.stereotype.Controller;
+////import org.springframework.web.bind.annotation.ModelAttribute;
+////import org.springframework.web.bind.annotation.RequestMapping;
+////import org.springframework.web.bind.annotation.RequestMethod;
+////
+////import com.moopi.mvc.service.domain.User;
+////import com.moopi.mvc.service.user.UserService;
+////
+////@Controller
+////@RequestMapping("/user/*")
+////public class UserController {
+////
+////	@Autowired
+////	@Qualifier("userServiceImpl")
+////	private UserService userService;
+////
+////	public UserController() {
+////		System.out.println(this.getClass());
+////	}
+////
+////	// @Value("#{commonProperties['pageUnit']}")
+////	// int pageUnit;
+////	// @Value("#{commonProperties['pageSize']}")
+////	// int pageSize;
+////
+////	// 로그인
+////	@RequestMapping(value = "login", method = RequestMethod.POST)
+////	public String login(@ModelAttribute("user") User user, HttpSession session) throws Exception {
+////		// 2021-07-08 12:37 : VO쪽 getter, setter Annotaion지정했으나, 끌고오지 못함 알아봐야함
+////		User dbUser = userService.getUser(user.getUserId());
+////		return "redirect:/index";
+////	}
+////
+////	// 로그아웃
+////	@RequestMapping(value = "logout", method = RequestMethod.GET)
+////	public String logout(HttpSession session) throws Exception {
+////		session.invalidate();
+////
+////		return "redirect:/main.jsp";
+////	}
+////
+////	// 회원가입
+////	@RequestMapping(value = "addUser", method = RequestMethod.POST)
+////	public String addUser() throws Exception {
+////
+////		// userService.addUser(user);
+////
+////		return "redirect:/user/addUserInfo.jsp";
+////	}
+////
+////	// 회원가입 이후 추가 정보입력
+////	@RequestMapping(value = "addUserInfo", method = RequestMethod.POST)
+////	public String addUserInfo() throws Exception {
+////		return "redirect:/main.jsp";
+////	}
+////
+//////	public void addUser(User user) throws Exception;
+//////	
+//////	public User getUser(String userId) throws Exception;
+//////	
+//////	public List<User> getUserList(Search search) throws Exception;
+//////	
+//////	public void updateUser(User user) throws Exception;
+//////	
+//////	public int getTotalCount(Search search) throws Exception;
+////
+////}
+//
---
+++ MoopiProject/src/test/java/com/moopi/mvc/MoimTest.java
@@ -0,0 +1,42 @@
+package com.moopi.mvc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.moopi.mvc.service.moim.impl.MoimServiceImpl;
+import com.moopi.mvc.service.domain.Moim;
+
+@SpringBootTest
+public class MoimTest {
+	
+	@Autowired
+	private MoimServiceImpl moimService;
+
+	
+	@Test
+	public void getMoim() {
+		
+		System.out.println("Test getMoim :::");
+		System.out.println(moimService.getMoim(1));
+		Moim moim = moimService.getMoim(1);
+		System.out.println(moim);
+		
+		Assertions.assertEquals("미사리조기축구회", moim.getMmName());
+		Assertions.assertEquals("공찹니다", moim.getMmContent());
+		
+	}
+	
+//	@Test
+//	public void updateMoim() {
+//		
+//		System.out.println("Test updateMoim :::");
+//		System.out.println("수정전 모임 : "+moimService.getMoim(1));
+//		Moim moim = moimService.getMoim(1);
+//		moim.setMmAddr("중랑구");
+//		moim.
+//		
+//		
+//	}
+}
