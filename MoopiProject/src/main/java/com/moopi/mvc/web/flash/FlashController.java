a8a111867c69ac5e4a9e2766b5cfd5a7e0a275c9
guddn <guddn@LAPTOP-65FJECOJ>
2021-07-09 17:08:46 +0900

    j commit  0709 17:08

--- MoopiProject/src/main/java/com/moopi/mvc/web/flash/FlashController.java
+++ MoopiProject/src/main/java/com/moopi/mvc/web/flash/FlashController.java
@@ -1,9 +1,38 @@
 package com.moopi.mvc.web.flash;
 
-public class FlashController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.moopi.mvc.service.domain.Flash;
+import com.moopi.mvc.service.flash.impl.FlashServiceImpl;
+
+@Controller
+@RequestMapping("/flash/*")
+public class FlashController {
+	
+	@Autowired
+	private FlashServiceImpl flashService;
+	
+	//Constructor
 	public FlashController() {
-		// TODO Auto-generated constructor stub
+		System.out.println(this.getClass());
 	}
 
+	@RequestMapping("getFlash")
+	public Flash getFlash(@RequestParam("flashNo") int flashNo, Model model) throws Exception {
+		
+		System.out.println("getFlash ::: start!!");
+		
+		Flash flash = flashService.getFlash(flashNo);
+		System.out.println(flash);
+		
+		model.addAttribute("flash", flashService.getFlash(flashNo));
+		
+		return flash;
+	}
+	
+	
 }
--- MoopiProject/src/test/java/com/moopi/mvc/MoopiProjectApplicationTests.java
+++ MoopiProject/src/test/java/com/moopi/mvc/MoopiProjectApplicationTests.java
@@ -1,13 +1,30 @@
 package com.moopi.mvc;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.moopi.mvc.service.domain.Flash;
+import com.moopi.mvc.service.flash.impl.FlashServiceImpl;
 
 @SpringBootTest
 class MoopiProjectApplicationTests {
 
+	@Autowired
+	private FlashServiceImpl flashService;
+	
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void getFlash() throws Exception {
+		System.out.println("Test getFlash() start ::");
+		System.out.println(flashService.getFlash(1));
+		Flash flash = flashService.getFlash(1);
+		
+		Assertions.assertEquals("hi", flash.getFlashName());
+	}
+	
 }
