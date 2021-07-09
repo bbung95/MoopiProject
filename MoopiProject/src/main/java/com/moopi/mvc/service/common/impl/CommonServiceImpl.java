9279093a1e89ae84992689052b3c54920f3ab110
bbung95 <tjd123123@naver.com>
2021-07-09 12:55:22 +0900

    2021-07-09 bbung
    중간커

--- MoopiProject/src/main/java/com/moopi/mvc/service/common/impl/CommonServiceImpl.java
+++ MoopiProject/src/main/java/com/moopi/mvc/service/common/impl/CommonServiceImpl.java
@@ -1,11 +1,29 @@
 package com.moopi.mvc.service.common.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.moopi.mvc.service.common.CommonDaoImpl;
+import com.moopi.mvc.service.domain.Notice;
 
 @Service
 public class CommonServiceImpl {
 
+	@Autowired
+	private CommonDaoImpl commonDao;
+	
 	public CommonServiceImpl() {
 	}
+	
+	public void addNotice(Notice noitce) {
+		
+		commonDao.addNotice(noitce);
+	}
+	
+	public Notice getNotice(int noticeNo) {
+		
+		return commonDao.getNotice(noticeNo);
+	}
+	
 
 }
