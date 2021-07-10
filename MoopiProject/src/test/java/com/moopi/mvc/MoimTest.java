69df42e6fc996c4ce7ceaa14015da750a0ea742b
treefrog18 <65808975+treefrog18@users.noreply.github.com>
2021-07-10 16:46:04 +0900

    CYU:단위테스트 완료

--- MoopiProject/src/test/java/com/moopi/mvc/MoimTest.java
+++ MoopiProject/src/test/java/com/moopi/mvc/MoimTest.java
@@ -1,11 +1,14 @@
 package com.moopi.mvc;
 
+import java.util.Map;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.moopi.mvc.service.moim.impl.MoimServiceImpl;
+import com.moopi.mvc.common.Search;
 import com.moopi.mvc.service.domain.Moim;
 
 @SpringBootTest
@@ -15,17 +18,17 @@ public class MoimTest {
 	private MoimServiceImpl moimService;
 
 	
-//	@Test
-//	public void getMoim() {
-//		
-//		System.out.println("Test getMoim :::");
-//		System.out.println(moimService.getMoim(1));
-//		Moim moim = moimService.getMoim(1);
-//		System.out.println(moim);
-//		
-//		Assertions.assertEquals("미사리조기축구회", moim.getMmName());
-//		Assertions.assertEquals("공찹니다", moim.getMmContent());	
-//	}
+	@Test
+	public void getMoim() {
+		
+		System.out.println("Test getMoim :::");
+		System.out.println(moimService.getMoim(1));
+		Moim moim = moimService.getMoim(1);
+		System.out.println(moim);
+		
+		Assertions.assertEquals("미사리조기축구회", moim.getMmName());
+		Assertions.assertEquals("공찹니다", moim.getMmContent());	
+	}
 	
 //	@Test
 //	public void updateMoim() throws Exception{
@@ -41,26 +44,56 @@ public class MoimTest {
 //		Assertions.assertEquals(96, moim.getMmMaxAge());
 //	}
 	
-	@Test
-	public void addMoim() throws Exception{
-		
-		System.out.println("Test addMoim :::");
-		Moim moim = new Moim();
-		moim.setMmNo(2);
-		moim.setMmName("YMCA족구단");
-		moim.setMmContent("불심으로 대동단결");
-		moim.setMmFile("송강호사진");
-		moim.setMmMaxCount(10);
-		moim.setMmCurrentCount(1);
-		moim.setMmInterest(2);
-		moim.setMmAddr("은평구");
-		moim.setMmState(2);
-		moim.setMmMinAge(35);
-		moim.setMmMaxAge(60);
-		moim.setMmType(2);
-		
-		moimService.addMoim(moim);
-		System.out.println(moim);
-	}
+//	@Test
+//	public void addMoim() throws Exception{
+//	      
+//      System.out.println("Test addMoim :::");
+//      Moim moim = new Moim();
+//      moim.setMmNo(17);
+//      moim.setMmConstructor("user01");
+//      moim.setMmName("식물갤러리");
+//      moim.setMmContent("식물도 하나의 생명입니다.");
+//      moim.setMmFile("난초");
+//      moim.setMmMaxCount(7);
+//      moim.setMmAddr("광진구");
+//      moim.setMmInterest(3);
+//      moim.setMmState(2);
+//      moim.setMmMinAge(30);
+//      moim.setMmMaxAge(87);
+//      moim.setMmType(1);
+//      
+//      moimService.addMoim(moim);
+//      System.out.println(moim);
+//   }
 	
+	
+	@Test
+	public void getMoimList() throws Exception{
+		
+		System.out.println("겟모임리스트~~ 테스트~~");
+		Search search = new Search();
+		search.setSearchCondition(1);
+		search.setSearchKeyword("2");
+		Map map = moimService.getMoimList(search);
+		System.out.println("인터레스트가 2인 모임의 리스트 : "+map.get("list"));
+		System.out.println("인터레스트가 2인 모임의 개수"+map.get("totalCount"));	
+		
+//		search.setSearchCondition(2);
+//		search.setSearchKeyword("중구");
+//		Map map = moimService.getMoimList(search);
+//		System.out.println("거주지가 중구인 모임의 리스트 : "+map.get("list"));
+//		System.out.println("거주지가 중구인 모임의 개수"+map.get("totalCount"));
+	}
+
+//	@Test
+//	public void getTotalCount() throws Exception{
+//		
+//		System.out.println("겟토탈카운트 테스트");
+//		Search search = new Search();
+//		search.setSearchCondition(2);
+//		search.setSearchKeyword("서초구");
+//		System.out.println("거주지가 서초구인 모임의 숫자 : "+moimService.getTotalCount(search));
+//		Assertions.assertEquals(1, moimService.getTotalCount(search));
+//	}
+
 }
