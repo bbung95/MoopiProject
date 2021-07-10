e91b0553ff252f648648ffb16251927585d9a075
bit <bit@DESKTOP-MNEP9O9>
2021-07-10 17:08:16 +0900

    ksh : 0710 17:08 commit;
    board mapper 구현중

--- MoopiProject/src/main/java/com/moopi/mvc/service/board/impl/BoardServiceImpl.java
+++ MoopiProject/src/main/java/com/moopi/mvc/service/board/impl/BoardServiceImpl.java
@@ -5,59 +5,50 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
 import com.moopi.mvc.common.Search;
 import com.moopi.mvc.service.board.BoardDao;
-import com.moopi.mvc.service.board.BoardService;
 import com.moopi.mvc.service.domain.Board;
 
 
 
-//@Service("boardServiceImpl")
-public class BoardServiceImpl implements BoardService {
+@Service
+public class BoardServiceImpl {
 
-//	@Autowired
+	@Autowired
 	private BoardDao boardDao;
 	
-	public void setBoardDao(BoardDao boardDao) {
-		this.boardDao = boardDao;
-	}
 	
-	public BoardServiceImpl(){
-		System.out.println(this.getClass());
-	}
-	
-	
-	@Override
-	public void addBoard(Board board) throws Exception {
-		// TODO Auto-generated method stub
-		boardDao.insertBoard(board);
-
-	}
-
-	@Override
-	public Board getBoard(int boardNo) throws Exception {
-		// TODO Auto-generated method stub
-		return boardDao.getBoard(boardNo);
-	}
-
-	@Override
-	public Map<String, Object> getBoardList(Search search) throws Exception {
+	public void addBoard(Board board)  {
 		
-		List<Board> list= boardDao.getBoardList(search);
-		int totalCount =  boardDao.getTotalCount(search); 
+		boardDao.addBoard(board);
+
+	}
+
+	
+	public Board getMoopiBoard(int boardNo){
 		
-		Map<String, Object> map = new HashMap<String, Object>();
-		System.out.println(search+" serviceimpl");
+		return boardDao.getMoopiBoard(boardNo);
+	}
+
+
+	public Map<String, Object> getBoardList(HashMap map){
+		
+		
+		List<Board> list= boardDao.getBoardList(map);
+//		int totalCount =  boardDao.getTotalCount(map); 
+		
+	
+
 		map.put("list", list);
-		map.put("totalCount", totalCount);
+//		map.put("totalCount", totalCount);
 		
 		return map;
 	}
 
-	@Override
-	public void updateBoard(Board board) throws Exception {
+
+	public void updateBoard(Board board){
 		
 		boardDao.updateBoard(board);
 
