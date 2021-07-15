0edc1265385d6717bc18ad9c62199ad6d5da51ac
bbung95 <tjd123123@naver.com>
2021-07-16 00:51:43 +0900

    2021-07-16 박성은 수정 1
    채팅 jsp 변경 및 채팅요청 구현,
    
    알림 로직 변경

--- test/src/main/java/com/moopi/mvc/web/common/CommonController.java
+++ test/src/main/java/com/moopi/mvc/web/common/CommonController.java
@@ -1,12 +1,22 @@
 package com.moopi.mvc.web.common;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.moopi.mvc.common.Search;
+import com.moopi.mvc.service.user.impl.UserServiceImpl;
 
 @Controller
 public class CommonController {
-
+	
+	@Autowired
+	private UserServiceImpl userService;
+	
 	public CommonController() {
 	}
 	
@@ -17,7 +27,7 @@ public class CommonController {
 		return "index";
 	}
 	
-	@GetMapping("/common/adminMoopi")
+	@GetMapping("/adminMoopi")
 	public String adminMoopi() {
 		
 		System.out.println("adminMoopi : GET");
@@ -25,11 +35,19 @@ public class CommonController {
 		return "common/adminMain";
 	}
 	
-	@RequestMapping("/list")
-	public String userList() {
-		
-		System.out.println("/userList");
-		return "userList";
+	//@CrossOrigin(origins = "http://localhost:82")
+	@GetMapping(value="/chat/chatList")
+	public String chatList() {
+		System.out.println("chatList : GET");
+		return "chat/chatList";
 	}
 	
+	@GetMapping(value="/chat/joinRoom")
+	public String joinRoom(@RequestParam("trgt") String trgt, Model model) {
+		
+		System.out.println("joinRoom : GET");
+		System.out.println(trgt);
+		model.addAttribute("target", trgt);
+		return "chat/chatRoom";
+	}
 }
